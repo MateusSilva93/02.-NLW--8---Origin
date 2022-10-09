1a9225f024d597fdd3f351f45070446c54b2266e
Mateus <stm_mateus@hotmail.com>
2022-10-09 20:47:14 -0300

    ajuste-designing

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
+window.addEventListener('scroll', onScroll)
+onScroll()
+
 function onScroll() {
+  showNavOnScroll()
+  showBackToTopOnScroll()
+}
+
+function showNavOnScroll() {
   if (scrollY == 0) {
     navig.classList.remove('scroll')
   } else {
     navig.classList.add('scroll')
+  }
+}
+
+function showBackToTopOnScroll() {
+  if (scrollY > 400) {
+    backToTop.classList.add('show')
+  } else {
+    backToTop.classList.remove('show')
   }
 }
 
@@ -13,3 +29,19 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
+
+ScrollReveal({
+  origin: 'top',
+  distance: '70px',
+  duration: 700
+}).reveal(`
+  #home, 
+  #home img, 
+  #home .stats, 
+  #services,
+  #services header,
+  #services .card,
+  #about-us, 
+  #about-us header, 
+  #about-us .content,
+  #contact`)

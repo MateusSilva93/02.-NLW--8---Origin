ae70aa45b56f6070d92fdbfab3b78062a94d419f
Mateus <stm_mateus@hotmail.com>
2022-10-12 10:04:51 -0300

    ajuste menu

--- main.js
+++ main.js
@@ -4,6 +4,36 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(aboutUs)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+  // Valor da linha central da tela
+  const targetLine = scrollY + innerHeight / 2
+
+  // Valores do limite da section
+  const sectionBegin = section.offsetTop
+  const sectionHeight = section.offsetHeight
+  const sectionEnd = sectionBegin + sectionHeight
+
+  //Validações de Posição
+  const targetLinePassedSectionBegin = targetLine >= sectionBegin
+  const targetLinePassedSectionEnd = targetLine > sectionEnd
+  const targetLineInSection =
+    targetLinePassedSectionBegin && !targetLinePassedSectionEnd
+
+  //Variavei do HTML
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (targetLineInSection) {
+    menuElement.classList.add('active')
+  }
 }
 
 function showNavOnScroll() {
